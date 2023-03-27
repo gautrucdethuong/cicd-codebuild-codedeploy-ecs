890c6601429682ac5813877aeb432077906f91c0
Huynh Nhat Minh <minhhuynh051120@gmail.com>
2023-03-28 01:07:48 +0700

    update file index config ip and name service

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express'
+import os from 'os'
 
 const app = express();
 
-app.get('/', (rew, res) => {
-    console.log('Hello world!');
-    res.json('Hello world!!!');
+app.get('/', (req, res) => {
+    const clientIp = req.header('x-forwarded-for');
+    const albIp = req.socket.remoteAddress;
+    const containerIp = req.socket.localAddress;
+    const containerName = os.hostname();
+    console.log('Hello Amazon ECS from Minhhn');
+    res.json({
+        serviceName: 'Minhhn ECS demo',
+        contact: 'study-aws@gmail.com',
+        clientIp: clientIp,
+        albIp: albIp,
+        containerIp: containerIp,
+        containerName: containerName,
+    });
 });
 
 app.listen(8080, ()=>{
